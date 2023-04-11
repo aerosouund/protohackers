d83b54f6ee62add5e2c0b9855cafc962069aa267
Ammar Yasser <yourgitemail@example.com>
2023-04-11 21:17:02 +0200

    mutex

--- budget-chat/main.go
+++ budget-chat/main.go
@@ -5,44 +5,96 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 )
 
-// TO DO
-/*
-1- on connection call the authorize function
-2- once authed add to an array of users
-3- implement send message
-4-
+type client struct {
+	conn net.Conn
+	name string
+}
 
-*/
+var (
+	clients   []*client
+	clientsMu sync.Mutex
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Send the "What is your name?" question to the client
+	fmt.Fprintf(conn, "What is your name?\n")
+
+	// Read the client's name
+	scanner := bufio.NewScanner(conn)
+	scanner.Scan()
+	name := scanner.Text()
+
+	fmt.Printf("Client '%s' has joined\n", name)
+
+	// Add the client to the list of active connections
+	client := &client{conn, name}
+	clientsMu.Lock()
+	clients = append(clients, client)
+	clientsMu.Unlock()
+
+	// Send a message to all connected clients
+	message := fmt.Sprintf("Client '%s' has joined", name)
+	sendToAll(message)
+
+	// Keep the connection alive
+	for {
+		// Read the client's input
+		scanner.Scan()
+		input := scanner.Text()
+
+		// If the client sends "quit", close the connection
+		if input == "quit" {
+			fmt.Printf("Client '%s' has left\n", name)
+			clientsMu.Lock()
+			// Remove the client from the list of active connections
+			for i, c := range clients {
+				if c.conn == conn {
+					clients = append(clients[:i], clients[i+1:]...)
+					break
+				}
+			}
+			clientsMu.Unlock()
+			// Send a message to all connected clients
+			message := fmt.Sprintf("Client '%s' has left", name)
+			sendToAll(message)
+			return
+		}
+
+		// Echo the client's input back to them
+		fmt.Fprintf(conn, "You said: %s\n", input)
+	}
+}
+
+func sendToAll(message string) {
+	clientsMu.Lock()
+	for _, client := range clients {
+		fmt.Fprintf(client.conn, "%s\n", message)
+	}
+	clientsMu.Unlock()
+}
 
 func main() {
-	listener, err := net.Listen("tcp", ":8000")
+	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		return
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
 	}
 	defer listener.Close()
 
-	fmt.Println("Listening on port 8000...")
+	fmt.Println("Server started. Listening on port 1234...")
 
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection:", err.Error())
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			continue
+		}
+		fmt.Printf("New connection from %s\n", conn.RemoteAddr().String())
+		go handleConnection(conn)
 	}
-	defer conn.Close()
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your name: ")
-	name, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Store the user's name in a variable
-	name = name[:len(name)-1] // remove the trailing newline character
-	fmt.Println("Received name:", name)
-
-	// Send the user's name to the client
-	conn.Write([]byte("Hello " + name))
 }
