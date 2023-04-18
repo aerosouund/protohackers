599fd0da117372ec632c94a199f53ba3097e1ba8
Ammar Yasser <yourgitemail@example.com>
2023-04-18 19:33:56 +0200

    discovered no new line character

--- mob-in-the-middle/main.go
+++ mob-in-the-middle/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -19,8 +20,9 @@ var (
 	clientsMu sync.Mutex
 )
 
-func handleConnection(conn net.Conn) {
+func handleConnection(conn net.Conn, upcon net.Conn) {
 	defer conn.Close()
+	defer upcon.Close()
 
 	fmt.Fprintf(conn, "Welcome to budgetchat! What shall I call you?\n")
 
@@ -34,6 +36,21 @@ func handleConnection(conn net.Conn) {
 			conn.Close()
 			return
 		}
+
+		gscanner := bufio.NewScanner(upcon)
+		for gscanner.Scan() {
+			textresponse := gscanner.Text()
+			fmt.Println("Response:", string(textresponse[len(textresponse)-1]))
+		}
+
+		fmt.Fprintf(upcon, name)
+
+		gscanner.Scan()
+		input := gscanner.Text()
+
+		// parse whats coming from the upstream
+		fmt.Println(input)
+
 		fmt.Printf("* '%s' has joined the room\n", name)
 
 		greetingMsg := "* This room contains "
@@ -45,15 +62,18 @@ func handleConnection(conn net.Conn) {
 		}
 
 		fmt.Fprintf(conn, greetingMsg+format, clientNames...)
+		fmt.Fprintf(upcon, greetingMsg+format, clientNames...)
 
 		// Add the client to the list of active connections
-		message := fmt.Sprintf("* '%s' has joined the room", name)
-		sendToAll(message)
+		message := fmt.Sprintf("* '%s' has joined the room\n", name)
 
 		client := &client{conn, name}
 		clientsMu.Lock()
 		clients = append(clients, client)
 		clientsMu.Unlock()
+
+		sendToAll(message)
+		fmt.Fprintf(upcon, message)
 
 	} else {
 		return
@@ -65,9 +85,11 @@ func handleConnection(conn net.Conn) {
 		res := scanner.Scan()
 		input := scanner.Text()
 
+		// handle connection closure
 		if res == false {
 			conn.Close()
-			fmt.Printf("* '%s' has left the room", name)
+			upcon.Close()
+			fmt.Printf("* '%s' has left the room\n", name)
 			clientsMu.Lock()
 			// Remove the client from the list of active connections
 			for i, c := range clients {
@@ -77,13 +99,26 @@ func handleConnection(conn net.Conn) {
 				}
 			}
 			clientsMu.Unlock()
-			message := fmt.Sprintf("* '%s' has left the room", name)
+			message := fmt.Sprintf("* '%s' has left the room\n", name)
+
 			sendToAll(message)
+			fmt.Fprintf(upcon, message)
 			return
 		}
 		if len(input) != 0 {
-			message := fmt.Sprintf("[%s] %s", name, input)
+			pattern := `\b7\w{25,34}\b`
+			re := regexp.MustCompile(pattern)
+			var message string
+
+			if re.MatchString(input) {
+				modified := re.ReplaceAllString(input, "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
+				message = fmt.Sprintf("[%s] %s", name, modified)
+			} else {
+				message = fmt.Sprintf("[%s] %s", name, input)
+			}
+			// send upstream
 			sendToAllExceptSender(conn, message)
+			fmt.Fprintf(upcon, message)
 		}
 	}
 }
@@ -123,6 +158,13 @@ func main() {
 			continue
 		}
 		fmt.Printf("New connection from %s\n", conn.RemoteAddr().String())
-		go handleConnection(conn)
+
+		upstreamConn, err := net.Dial("tcp", "chat.protohackers.com:16963")
+		if err != nil {
+			fmt.Printf("Error in creating upstream: %s\n", err)
+			continue
+		}
+
+		go handleConnection(conn, upstreamConn)
 	}
 }
